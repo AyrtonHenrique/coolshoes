03270f7f1065af633c4f3b221224482fadeb1e21
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2020-09-30 20:18:16 -0300

    basic auth

--- src/test/java/br/com/fiap/coolshoes/CoolshoesApplicationTests.java
+++ src/test/java/br/com/fiap/coolshoes/CoolshoesApplicationTests.java
@@ -3,11 +3,7 @@ package br.com.fiap.coolshoes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CoolshoesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
