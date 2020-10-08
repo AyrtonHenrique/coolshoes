fe909fb480107393d6443ef43a7b944d58e6aa2c
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2020-10-07 21:01:02 -0300

    batch

---
+++ coolshoesbatch/src/test/java/br/com/fiap/coolshoesbatch/CoolshoesbatchTaskletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.coolshoesbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolshoesbatchTaskletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
